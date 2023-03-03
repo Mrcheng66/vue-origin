d7541751bb3455bab5669e14b28bd646d085b16e
hao.cheng <hao.cheng@71360.com>
2023-03-03 13:45:30 +0800

    'creatElm'

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,6 +1,57 @@
 import { createElementVNode, createTextVNode } from "./vdom/index"
 
-function patch(vnode, el) {
+function createElm(vnode) {
+  let { tag, data, children, text } = vnode
+  if (typeof tag == 'string') { // 标签
+    vnode.el = document.createElement(tag) // 这里将真实的节点和虚拟节点对应起来, 后续如果修改了属性
+    // 处理属性
+    patchProps(vnode.el, data)
+    // 递归添加子节点dom
+    children.forEach(child => {
+      vnode.el.appendChild(createElm(child))
+    });
+  } else { // 文本节点
+    vnode.el = document.createTextNode(text)
+  }
+  return vnode.el
+}
+
+function patchProps(el, props) {
+  for (const key in props) {
+    if (key == 'style') {
+      for (const styleName in props.style) {
+        el.style[styleName] = props.style[styleName]
+      }
+    } else {
+      el.setAttribute(key, props[key])
+    }
+  }
+}
+
+export function patch(oldVnode, vnode) {
+  const isRealElement = oldVnode.nodeType
+  if (isRealElement) {
+    // 如果是真实dom (初渲染)
+    const elm = oldVnode
+
+    const parenElm = elm.parentNode; // 拿到父元素
+
+    const newElm = createElm(vnode)
+    // console.log(newElm)
+    /* 
+      先插入新节点 el，再删除老的 el 是为了避免在插入新节点之前，
+      一些 DOM 元素被脚本等异步操作修改，导致渲染不准确的问题。
+
+      如果先删除老的 el 再插入新的 el，那么在这个过程中，如果出现了异步操作，
+      可能会对老的 el 进行修改，导致渲染不准确。而如果先插入新的 el，再删除老的 el，
+      就能保证新的 el 能够正常渲染，避免了这个问题。
+      同时，Vue 在插入新的 el 时，也会保证它处于合适的位置，避免了插入位置的错误
+    */
+    parenElm.insertBefore(newElm, elm.nextSibling) // 先插入新的 el
+    parenElm.removeChild(elm) // 删除老的 el
+  } else {
+    // diff算法
+  }
   console.log(vnode)
 }
 
@@ -25,9 +76,7 @@ export function initLifeCycle(Vue) {
   }
 
   Vue.prototype._s = function (value) {
-    if (typeof value !== 'object') {
-      return value
-    }
+    if (typeof value !== 'object') return value
     return JSON.stringify(value)
   }
 
