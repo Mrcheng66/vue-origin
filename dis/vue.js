edca756e23de5adc6e94794e4f006e196a5c905e
MrCheng <89339841@qq.com>
2023-05-08 00:05:46 +0800

    计算属性完成

--- dis/vue.js
+++ dis/vue.js
@@ -35,6 +35,17 @@
 
   Dep.target = null;
 
+  let stack = [];
+  function pushTarget(watcher) {
+    stack.push(watcher);
+    Dep.target = watcher;
+  }
+
+  function popTarget() {
+    stack.pop();
+    Dep.target = stack[stack.length - 1];
+  }
+
   // 1 当我们创建渲染watcher的时候 我们会把当前的渲染watcher 放到Dep.target 上
   // 2 调用_render() 会取值 走到get上
 
@@ -49,13 +60,29 @@
       this.getter = callback; // geter 意味着调用这个函数可以触发取值操作
       this.deps = []; // 让watcher记住dep也是为了组件卸载和计算属性的实现
       this.depsId = new Set();
-      this.get();
+      
+      this.lazy = options.lazy;
+      this.dirty = this.lazy; // 缓存值
+      this.lazy ? undefined : this.get();
     }
-
+    // 判断dirty重新执行
+    evaluate() {
+      this.value = this.get(); // 获取用户函数的返回值，并且还要标记为脏
+      this.dirty = false;
+    }
     get() {
-      Dep.target = this; // 静态属性就是只有一份
-      this.getter(); // 会去vm上取值
-      Dep.target = null; // 渲染完毕就清空（清空是为了保证只有在模板里面才收集，在vm上获取属性是不收集的）
+      // Dep.target = this // 静态属性就是只有一份
+      pushTarget(this);
+      let value = this.getter.call(this.vm); // 会去vm上取值
+      // Dep.target = null // 渲染完毕就清空（清空是为了保证只有在模板里面才收集，在vm上获取属性是不收集的）
+      popTarget();
+      return value
+    }
+    depend() {
+      let i = this.deps.length;
+      while (i--) {
+        this.deps[i].depend(); // 让计算属性watcher 也收集渲染watcher
+      }
     }
 
     addDep(dep) {
@@ -68,8 +95,13 @@
     }
 
     update() {
-      // this.get() // 重新渲染更新 （不能直接同步更新，多次set值会重复渲染）
-      queueWatcher(this);
+      if (this.lazy) {
+        // 如果是计算属性 依赖的值变化了 就标记计算属性是脏值了
+        this.dirty = true;  
+      } else {
+        // this.get() // 重新渲染更新 （不能直接同步更新，多次set值会重复渲染）
+        queueWatcher(this);
+      }
     }
 
     run() {
@@ -648,17 +680,20 @@
   function initState(vm) {
     // 获取所有选项
     const opts = vm.$options;
-
     if (opts.data) {
+      // 数据
       initData(vm);
     }
-
+    if (opts.computed) {
+      // 计算属性
+      initComputed(vm);
+    }
   }
 
   function initData(vm) {
     let data = vm.$options.data; // data可能是函数或者对象
 
-    data = typeof data === 'function' ? data.call(vm) : data;
+    data = typeof data === "function" ? data.call(vm) : data;
 
     // 下面的方法进行了属性 劫持但是在vm实例上并不会有data 属性直接访问 就是不能直接像项目里面 this.** 访问数据， 所以额外定义一个参数
     vm._data = data; // 这样又不便于直接的操作， 所以额外设置一层 _data的代理
@@ -666,18 +701,62 @@
     observe(data);
 
     // 将vm._data 用vm来代理
-    Object.keys(data).forEach(key => proxy(vm, '_data', key));
+    Object.keys(data).forEach((key) => proxy(vm, "_data", key));
   }
 
   function proxy(vm, target, key) {
     Object.defineProperty(vm, key, {
       get() {
-        return vm[target][key]
+        return vm[target][key];
       },
       set(newVal) {
         vm[target][key] = newVal;
-      }
+      },
     });
+  }
+
+  function initComputed(vm) {
+    const computed = vm.$options.computed;
+    const watcher = vm._computedWatchers = {};
+    for (const key in computed) {
+      if (Object.hasOwnProperty.call(computed, key)) {
+        const useDef = computed[key];
+        
+        // 我们需要监控 计算属性中get的变化
+        const fn = typeof useDef === "function" ? useDef : useDef.get;
+        // 如果直接new Watcher 默认就会执行fn， 所以在options中增加一个标识lazy
+        // 将属性和watcher对应起来
+        watcher[key] = new Watcher(vm, fn, {lazy: true});
+
+        defineComputed(vm, key, useDef);
+      }
+    }
+  }
+
+  function defineComputed(target, key, useDef) {
+    typeof useDef === "function" ? useDef : useDef.get;
+    const setter = useDef.set || (() => {});
+    // console.log(getter, setter);
+    Object.defineProperty(target, key, {
+      get: creatComputedGetter(key),
+      set: setter,
+    });
+  }
+
+  // 计算属性根本不会收集依赖，只会让自己的依赖属性去收集依赖
+  function creatComputedGetter(key) {
+    // 我们需要监测是否要执行这个getter
+    return function () {
+      const watcher = this._computedWatchers[key]; // 获取对应属性的watcher
+      if (watcher.dirty) {
+        // 如果是脏的就去执行用户传入的函数
+        watcher.evaluate(); // 直接掉get不合适因为存在值变化重新执行的情况
+      }
+      if (Dep.target) { // 计算属性出栈后 还要渲染watcher  我应该让计算属性watcher里面的属性也去收集上层watcher（渲染watcher）
+          watcher.depend();      
+      }
+      return watcher.value // 最后返回的是watcher上的值
+    }
   }
 
   const strats = {};
